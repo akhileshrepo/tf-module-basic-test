40324fcf33c83162b7fd87e9da301530ac631fb1
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-17 13:34:42 +0530

    updated code

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "null_resource" "ansible" {
 
   provisioner "local-exec" {
     command = <<EOF
-cd /home/centos/roboshop-ansible
+cd /home/centos/test
 git pull
 sleep 30
 ansible-playbook -i ${var.name}-dev.akhildevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
