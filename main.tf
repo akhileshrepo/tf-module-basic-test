09e2292b4e2f75373d606d1c46300c9d0ee5bfc0
AKhilesh <kothapallyakhil123@gmail.com>
2024-05-19 10:13:15 +0530

    updated code

--- main.tf
+++ main.tf
@@ -16,21 +16,3 @@ resource "aws_route53_record" "record" {
   ttl      = 30
   records  = [aws_instance.instance.private_ip]
 }
-
-
-
-resource "null_resource" "ansible" {
-
-  depends_on = [
-    aws_route53_record.record
-  ]
-
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/sample
-git pull
-sleep 30
-ansible-playbook -i ${var.name}-dev.akhildevops.online, main.yml -e ansible_user=centos -e ansible_password=DevOps321 -e component=${var.name}
-EOF
-  }
-}
